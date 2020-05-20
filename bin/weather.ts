d674ce70f07eb9ffcb0063709ea78b550b2db186
Ebuka Umeokonkwo <ebukaume@gmail.com>
2020-05-20 14:27:30 +0100

    feat: add validation for user input

--- bin/weather.ts
+++ bin/weather.ts
@@ -1,8 +1,14 @@
 import fetch from 'node-fetch'
-import { cleanInput, fetchCityWeather, processWeatherPromises, processValidResponse } from '../src/api'
+import { emptyInput, cleanInput, fetchCityWeather, processWeatherPromises, processValidResponse } from '../src/api'
 
 const commandLineArguments = process.argv
 const userInputs: Array<string> = commandLineArguments.splice(2)
+
+const emptyUserInput: boolean = emptyInput(userInputs)
+if(emptyUserInput) {
+  console.log('Please supply an input. Eg: npm start berlin')
+  process.exit(0)
+}
 
 const cleanedInput: Array<string> = cleanInput(userInputs)
 const cityWeatherPromises: Array<Promise<any>> = cleanedInput.map(city => fetchCityWeather(city, fetch))
